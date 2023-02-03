72d65b2dcef8e2cf4a620da35bc276d2f7354e13
Risi Ferraz <risiferraz@gmail.com>
2023-02-03 18:28:49 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const selecaoDeOpcoes=[ "opcao1", "opcao2", "opcao3", "opcao4", "opcao5", "opcao6"]
+//se a opçao for selecionada (clicada) remover da lista tranformando style em opcao-nula
+//considerar que a opção da página em questão já está como "opcao-nula"
+//dúvida: ao passar para a página do link, as opções clicadas deverão permanecer "opao-nulas"
